9ce4abcad859f214774e032f6c53515f88768aa6
Dmitry <118049037+mitruee@users.noreply.github.com>
2023-02-19 14:32:45 +0300

    Update MainActivity.java

--- app/src/main/java/com/example/applicationluckyticket/MainActivity.java
+++ app/src/main/java/com/example/applicationluckyticket/MainActivity.java
@@ -10,45 +10,36 @@ import android.widget.RadioButton;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    
+    private EditText ticketNumberIn;
+    private Button button;
+    private TextView ticketInfoOut;
 
-    // создание полей для вывода на экран нужных значений
-    private EditText ticketNumberIn; // поле ввода номера билета
-    private Button button; // кнопка обработки информации
-    private TextView ticketInfoOut; // поле вывода результирующей информации
+    private String ticketNumber = "";
 
-    // создание поля для алгоритма
-    private String ticketNumber = ""; // переменная для записи входного значения
-
-    // создадим объект нашего алгоритма, чтобы мы могли получить доступ к его методам
     Algorithm algorithm = new Algorithm();
 
-    // вывод на экран полученных значений
     @Override
-    protected void onCreate(Bundle savedInstanceState) { // создание жизненного цикла активности
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main); // присваивание жизненному циклу активити представления activity_main
+        setContentView(R.layout.activity_main);
 
-        // присваивание переменным активити элементов представления activity_main
-        ticketNumberIn = findViewById(R.id.ticketNumberIn); // поле ввода
-        button = findViewById(R.id.button); // кнопка обработки
-        ticketInfoOut = findViewById(R.id.ticketInfoOut); // поле вывода
+        ticketNumberIn = findViewById(R.id.ticketNumberIn);
+        button = findViewById(R.id.button);
+        ticketInfoOut = findViewById(R.id.ticketInfoOut);
 
-        // выполнение действий при нажании кнопки
-        button.setOnClickListener(listener); // обработка нажатия кнопки
+        button.setOnClickListener(listener);
     }
 
-    // объект обработки нажатия кнопки
     private View.OnClickListener listener = new View.OnClickListener() {
         @Override
         public void onClick(View view) {
 
-            // считывание введённого значения
             ticketNumber = ticketNumberIn.getText().toString();
 
-            // использование алгоритма для определения счастливого билета
-            if(algorithm.isHappyTicket(ticketNumber)) { // если номер билета счастливый, то об этом выводится информация на экран
+            if(algorithm.isHappyTicket(ticketNumber)) {
                 ticketInfoOut.setText("Данный номер билета счастливый " + algorithm.nextHappyTicket(ticketNumber));
-            } else { // иначе, производится поиск данного билета с последующим выводом на экран
+            } else {
                 ticketInfoOut.setText("Данный номер билета не счастливый, следующим счаcтливым номером является "
                         + algorithm.nextHappyTicket(ticketNumber));
             }
